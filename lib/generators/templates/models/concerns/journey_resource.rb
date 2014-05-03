b2fd9d1c6bfd20b587fc78ac0d709600b009ec99
Carl Kritzinger <carl@fireid.com>
2014-05-03 10:27:59 +0200

    initializers

--- lib/generators/templates/models/concerns/journey_resource.rb
+++ lib/generators/templates/models/concerns/journey_resource.rb
@@ -2,10 +2,12 @@ class JourneyResource < ActiveResource::Base
 
   def self.set_username(u)
   	@@username = u
+    self.user = @@username
   end
   
   def self.set_password(p)
   	@@password = p
+    self.password = @@password
   end
 
   def self.set_environment(e)
@@ -19,12 +21,10 @@ class JourneyResource < ActiveResource::Base
   	else
   		raise "environment should be :testing, :staging or :production"
   	end
-
+    self.site = "https://#{@@environment}.journeyapps.com/api/v1/_datamodel.json"
   end
 
   self.format = :json
   self.include_root_in_json = true
-  self.site = "https://run-testing.journeyapps.com/api/v1/_datamodel.json"
-  self.user = @@username
-  self.password = @@password
+
 end
